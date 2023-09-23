bb92dfbae9cce05dbeeab3097991c26dce93dd9b
Ffiirrsstt <first.engineer15@gmail.com>
2023-09-23 23:55:45 +0700

    update

---
+++ Homework Assignment 2/Homework Assignment 2/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Homework_Assignment_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.txtOutput = new System.Windows.Forms.GroupBox();
+            this.imgOutput = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.check = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.txtOutput.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOutput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1153, 773);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.Image = ((System.Drawing.Image)(resources.GetObject("player.Image")));
+            this.player.Location = new System.Drawing.Point(24, 705);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(97, 80);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 1;
+            this.player.TabStop = false;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Controls.Add(this.imgOutput);
+            this.txtOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.txtOutput.Location = new System.Drawing.Point(1188, 532);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(303, 253);
+            this.txtOutput.TabIndex = 2;
+            this.txtOutput.TabStop = false;
+            this.txtOutput.Text = "ผลลัพธ์";
+            // 
+            // imgOutput
+            // 
+            this.imgOutput.Image = ((System.Drawing.Image)(resources.GetObject("imgOutput.Image")));
+            this.imgOutput.Location = new System.Drawing.Point(70, 53);
+            this.imgOutput.Name = "imgOutput";
+            this.imgOutput.Size = new System.Drawing.Size(169, 154);
+            this.imgOutput.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgOutput.TabIndex = 0;
+            this.imgOutput.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.button1.Location = new System.Drawing.Point(1188, 394);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(303, 91);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "คลิกเพื่อทอยลูกเต๋า";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // check
+            // 
+            this.check.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.check.Location = new System.Drawing.Point(1244, 39);
+            this.check.Name = "check";
+            this.check.Size = new System.Drawing.Size(258, 309);
+            this.check.TabIndex = 4;
+            this.check.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1520, 858);
+            this.Controls.Add(this.check);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.txtOutput.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imgOutput)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.GroupBox txtOutput;
+        private System.Windows.Forms.PictureBox imgOutput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label check;
+    }
+}
+
---
+++ Homework Assignment 2/Homework Assignment 2/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework_Assignment_2
+{
+    public partial class Form1 : Form
+    {
+
+        Random random = new Random();
+        int dice,positionX, positionY, playerPoints = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            /*check.Text = "X: " + player.Location.X + ", Y: " + player.Location.Y
+            +"\r\nX: " + pictureBox2.Location.X + ", Y: " + pictureBox2.Location.Y
+            + "\r\nX: " + pictureBox3.Location.X + ", Y: " + pictureBox3.Location.Y
+            + "\r\nX: " + pictureBox4.Location.X + ", Y: " + pictureBox4.Location.Y
+            + "\r\nX: " + pictureBox5.Location.X + ", Y: " + pictureBox5.Location.Y
+            + "\r\nX: " + pictureBox6.Location.X + ", Y: " + pictureBox6.Location.Y
+            + "\r\nX: " + pictureBox7.Location.X + ", Y: " + pictureBox7.Location.Y
+            + "\r\nX: " + pictureBox8.Location.X + ", Y: " + pictureBox8.Location.Y
+            + "\r\nX: " + pictureBox9.Location.X + ", Y: " + pictureBox9.Location.Y
+            + "\r\nX: " + pictureBox10.Location.X + ", Y: " + pictureBox10.Location.Y;*/
+        }
+
+        private void calculateYPosition(object sender, EventArgs e)
+        {
+            if (playerPoints >= 1 && playerPoints <= 10)
+                positionY = 573;
+            else if (playerPoints >= 11 && playerPoints <= 20)
+                positionY = 511;
+            else if (playerPoints >= 21 && playerPoints <= 30)
+                positionY = 449;
+            else if (playerPoints >= 31 && playerPoints <= 40)
+                positionY = 386;
+            else if (playerPoints >= 41 && playerPoints <= 50)
+                positionY = 323;
+            else if (playerPoints >= 51 && playerPoints <= 60)
+                positionY = 260;
+            else if (playerPoints >= 61 && playerPoints <= 70)
+                positionY = 196;
+            else if (playerPoints >= 71 && playerPoints <= 80)
+                positionY = 134;
+            else if (playerPoints >= 81 && playerPoints <= 90)
+                positionY = 73;
+            else
+                positionY = 10;
+        }
+
+        private void walk(object sender, EventArgs e)
+        {
+            playerPoints += dice;
+            if ((playerPoints >= 1 && playerPoints <= 10) || (playerPoints >= 21 && playerPoints <= 30)||
+                (playerPoints >= 41 && playerPoints <= 50) || (playerPoints >= 61 && playerPoints <= 70) ||
+                (playerPoints >= 81 && playerPoints <= 90))
+                if(playerPoints % 10==0)
+                    positionX = 795;
+                else
+                    positionX = 20 + (86*((playerPoints%10) - 1));
+            else
+                positionX = 20 + (86 * (10-(playerPoints % 10)));
+            calculateYPosition(sender, e);
+            player.Location = new Point(positionX, positionY);
+            check.Text = "position : "+playerPoints +"\r\ndice : "+dice+ "\r\nX: " + player.Location.X + ", Y: " + player.Location.Y;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dice = random.Next(1, 7); 
+            string imagePath = Path.Combine(Application.StartupPath, "dice"+dice+".jpg");
+            imgOutput.Image = Image.FromFile(imagePath);
+            walk(sender, e);
+        }
+    }
+}
